50958f9412e1d0454047ea15d0d2ef139837121d
Gdisbar <115510224+Gdisbar@users.noreply.github.com>
2022-10-11 19:45:32 +0530

    Update ship_wirhin_D_days.cpp

--- sorting/ship_wirhin_D_days.cpp
+++ sorting/ship_wirhin_D_days.cpp
@@ -26,52 +26,52 @@ return left;
 
 1011. Capacity To Ship Packages Within D Days
 ===================================================
-A conveyor belt has packages that must be shipped from one port to another 
-within days days.The i-th package on the conveyor belt has a weight of weights[i]. 
-Each day, we load the ship with packages on the conveyor belt (in the order given by 
-weights). We may not load more weight than the maximum weight capacity of the ship.
+// A conveyor belt has packages that must be shipped from one port to another 
+// within days days.The i-th package on the conveyor belt has a weight of weights[i]. 
+// Each day, we load the ship with packages on the conveyor belt (in the order given by 
+// weights). We may not load more weight than the maximum weight capacity of the ship.
 
-Return the least weight capacity of the ship that will result in all the packages on 
-the conveyor belt being shipped within days days.
+// Return the least weight capacity of the ship that will result in all the packages on 
+// the conveyor belt being shipped within days days.
 
- // Translation : split the array into d  non-empty continuous subarrays and 
-//  minimize the largest sum(capacity) among these d subarrays(to ship all packages)
+//  // Translation : split the array into d  non-empty continuous subarrays and 
+// //  minimize the largest sum(capacity) among these d subarrays(to ship all packages)
 
-Example 1:
+// Example 1:
 
-Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
-Output: 15
-Explanation: A ship capacity of 15 is the minimum to ship all the packages in 5 
-days like this:
-1st day: 1, 2, 3, 4, 5
-2nd day: 6, 7
-3rd day: 8
-4th day: 9
-5th day: 10
+// Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
+// Output: 15
+// Explanation: A ship capacity of 15 is the minimum to ship all the packages in 5 
+// days like this:
+// 1st day: 1, 2, 3, 4, 5
+// 2nd day: 6, 7
+// 3rd day: 8
+// 4th day: 9
+// 5th day: 10
 
-Note that the cargo must be shipped in the order given, so using a ship of capacity 
-14 and splitting the packages into parts like (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) 
-is not allowed. -->// basically means continuous subarray
+// Note that the cargo must be shipped in the order given, so using a ship of capacity 
+// 14 and splitting the packages into parts like (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) 
+// is not allowed. -->// basically means continuous subarray
 
-Example 2:
+// Example 2:
 
-Input: weights = [3,2,2,4,1,4], days = 3
-Output: 6
-Explanation: A ship capacity of 6 is the minimum to ship all the packages in 3 days 
-like this:
-1st day: 3, 2
-2nd day: 2, 4
-3rd day: 1, 4
+// Input: weights = [3,2,2,4,1,4], days = 3
+// Output: 6
+// Explanation: A ship capacity of 6 is the minimum to ship all the packages in 3 days 
+// like this:
+// 1st day: 3, 2
+// 2nd day: 2, 4
+// 3rd day: 1, 4
 
-Example 3:
+// Example 3:
 
-Input: weights = [1,2,3,1,1], days = 4
-Output: 3
-Explanation:
-1st day: 1
-2nd day: 2
-3rd day: 3
-4th day: 1, 1
+// Input: weights = [1,2,3,1,1], days = 4
+// Output: 3
+// Explanation:
+// 1st day: 1
+// 2nd day: 2
+// 3rd day: 3
+// 4th day: 1, 1
 
 // 97% faster , 94% less memory
 //TC : n(for finding max & sum) + n*log(n) [sum calculation * inside binary search]
@@ -160,60 +160,60 @@ public:
 
 410. Split Array Largest Sum / book allocation problem
 ================================================================
-Given an array nums which consists of non-negative integers and an integer stud, 
-you can split the array into m non-empty continuous subarrays.
+// Given an array nums which consists of non-negative integers and an integer stud, 
+// you can split the array into m non-empty continuous subarrays.
 
-Write an algorithm to minimize the largest sum among these m subarrays.
+// Write an algorithm to minimize the largest sum among these m subarrays.
 
  
 
-Example 1:
+// Example 1:
 
-Input: nums = [7,2,5,10,8], m = 2
-Output: 18
-Explanation:
-There are four ways to split nums into two subarrays.
-The best way is to split it into [7,2,5] and [10,8],
-where the largest sum among the two subarrays is only 18.
+// Input: nums = [7,2,5,10,8], m = 2
+// Output: 18
+// Explanation:
+// There are four ways to split nums into two subarrays.
+// The best way is to split it into [7,2,5] and [10,8],
+// where the largest sum among the two subarrays is only 18.
 
-Example 2:
+// Example 2:
 
-Input: nums = [1,2,3,4,5], m = 2
-Output: 9
+// Input: nums = [1,2,3,4,5], m = 2
+// Output: 9
 
-Example 3:
+// Example 3:
 
-Input: nums = [1,4,4], m = 3
-Output: 4
+// Input: nums = [1,4,4], m = 3
+// Output: 4
 
 // Exact same as above 
 
 
 410. Split Array Largest Sum
 ==============================
-Given an array nums which consists of non-negative integers and an integer m, 
-you can split the array into m non-empty continuous subarrays.
+// Given an array nums which consists of non-negative integers and an integer m, 
+// you can split the array into m non-empty continuous subarrays.
 
-Write an algorithm to minimize the largest sum among these m subarrays.
+// Write an algorithm to minimize the largest sum among these m subarrays.
 
  
 
-Example 1:
+// Example 1:
 
-Input: nums = [7,2,5,10,8], m = 2
-Output: 18
-Explanation:
-There are four ways to split nums into two subarrays.
-The best way is to split it into [7,2,5] and [10,8],
-where the largest sum among the two subarrays is only 18.
+// Input: nums = [7,2,5,10,8], m = 2
+// Output: 18
+// Explanation:
+// There are four ways to split nums into two subarrays.
+// The best way is to split it into [7,2,5] and [10,8],
+// where the largest sum among the two subarrays is only 18.
 
-Example 2:
+// Example 2:
 
-Input: nums = [1,2,3,4,5], m = 2
-Output: 9
+// Input: nums = [1,2,3,4,5], m = 2
+// Output: 9
 
-Example 3:
+// Example 3:
 
-Input: nums = [1,4,4], m = 3
-Output: 4
+// Input: nums = [1,4,4], m = 3
+// Output: 4
 
