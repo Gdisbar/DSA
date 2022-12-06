581e2e80d23ca110c98609bfb9b54044787da196
Gdisbar <115510224+Gdisbar@users.noreply.github.com>
2022-12-06 11:21:53 +0530

    Add files via upload

---
+++ cp_template1.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x); cerr<<endl;
+#else
+#define debug(x)
+#endif
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define show(x) for(auto i : x)cout<<i<<endl;
+#define fill(x, y)     memset(x, y, sizeof(y))
+#define all(x)         (x).begin(), (x).end()
+#define sci(x)         int x;       scanf("%d", &x);
+#define scii(x, y)     int x, y;    scanf("%d %d", &x, &y);
+#define sciii(x, y, z) int x, y, z; scanf("%d %d %d", &x, &y, &z);
+#define TC(x)          sci(x); while(x --)
+#define eprintf(...)   fprintf(stderr, __VA_ARGS__)
+#define rep(i, x, y)   for (__typeof(x) i = x; i <= y; i ++)
+#define repi(i, x, y)  for (__typeof(x) i = x; i >= y; i --)
+#define fore(itr, x)   for (__typeof(x.begin()) itr = x.begin(); itr != x.end(); itr ++)
+#define forei(itr, x)  for (__typeof(x.end()) itr = x.end() - 1; itr != x.begin() - 1; itr --)
+
+
+typedef long long      ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int, int> ii;
+typedef pair<ii, int>  iii;
+typedef vector<int>    vi;
+typedef vector<ii>     vii;
+typedef vector<iii>    viii;
+
+
+const   int    inf = 0;
+const   double eps = 0;
+const   int  ms  = 0;
+const   int  md  = 0;
+
+
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+void solve()
+{
+	
+}
+int main(void)
+{
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+   freopen("error.txt", "w", stderr);
+#endif
+   solve();
+   return 0;
+}
